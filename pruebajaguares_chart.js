9856742725a8957c81d245907135c5a3cf4eafcf
leyner555 <leyner.rubik@gmail.com>
2023-06-28 04:00:08 -0500

    coordinates of the farm

---
+++ pruebajaguares_chart.js
@@ -0,0 +1,16 @@
+// Jaguar points
+var points = [
+    { 
+     geometry: ee.Geometry.Point([-76.796643, 0.856920]),
+     date: ee.Date('2023-01-01')
+   } ,{ 
+     geometry: ee.Geometry.Point([-76.791643, 0.872172]), 
+     date: ee.Date('2023-01-03') //  'yyyy-mm-dd'
+   }
+   
+   
+   /// Add more points
+ ];
+ 
+ 
+ exports.points = points;
